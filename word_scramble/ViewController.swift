e26dbdee92b4efdb53fc3530434587a11c05ba63
chibitofu <erinmoony@gmail.com>
2017-10-05 16:10:27 -0700

    Initial Commit

---
+++ word_scramble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  word_scramble
+//
+//  Created by Erin Moon on 10/5/17.
+//  Copyright © 2017 Erin Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
