49011550e6aea15f0506bc5a6a9573bf6e31c428
chibitofu <erinmoony@gmail.com>
2017-10-11 16:01:19 -0700

    Refactored alert messages.

--- word_scramble/ViewController.swift
+++ word_scramble/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
     
-    //Shuffles the words in allWords array using Gameplaykit, sets the title to the selected word and empties out the usedWords array.
+//Shuffles the words in allWords array using Gameplaykit, sets the title to the selected word and empties out the usedWords array.
     func startGame() {
         allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
         title = allWords[0]
@@ -21,12 +21,18 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    //Checks then submits the user answer to the usedWords array and adds it to the tableView.
+//Sends an alert message to the UI.
+    func alertMessage(errorTitle: String, errorMessage: String) -> Void {
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
+        return
+    }
+    
+//Checks then submits the user answer to the usedWords array and adds it to the tableView.
     func submit(answer: String) {
         let lowerAnswer = answer.lowercased()
-        
-        let errorTitle: String
-        let errorMessage: String
         
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
@@ -39,46 +45,41 @@ class ViewController: UITableViewController {
                     
                     return
                 } else {
-                    errorTitle = "Word not recognized"
-                    errorMessage = "You can't just make them up, you know!"
+                    alertMessage(errorTitle: "Word not recognized", errorMessage: "You can't just make them up, you know!")
                 }
             } else {
-                errorTitle = "Word used already"
-                errorMessage = "Be more original!"
+                alertMessage(errorTitle: "Word used already", errorMessage: "Be more original!")
             }
         } else {
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from '\(title!.lowercased())'!"
+            alertMessage(errorTitle: "Word not possible", errorMessage: "You can't spell '\(answer)' from '\(title!.lowercased())'!")
         }
-        
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
     }
     
-    //Checks to see if the user answer is an anagram of the word.
+//Checks to see if the user answer is an anagram of the word and longer than 3 letters.
     func isPossible(word: String) -> Bool {
         var tempWord = title!.lowercased()
         
-        for letter in word {
-            if let pos = tempWord.range(of: String(letter)) {
-                
-                tempWord.remove(at: pos.lowerBound)
-            } else {
-                return false
+        if tempWord.count > 3{
+            for letter in word {
+                if let pos = tempWord.range(of: String(letter)) {
+                    
+                    tempWord.remove(at: pos.lowerBound)
+                } else {
+                    return false
+                }
             }
         }
         
         return true
     }
     
-    //Checks to see if the user answer is a copy of a word in the usedWords array.
+//Checks to see if the user answer is a copy of a word in the usedWords array.
     func isOriginal(word: String) -> Bool {
         return !usedWords.contains(word)
     }
     
     
-    //Checks to see if the user answer is a real word in english.
+//Checks to see if the user answer is a real word in english.
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSMakeRange(0, word.utf16.count)
@@ -87,8 +88,8 @@ class ViewController: UITableViewController {
         return misspelledRange.location == NSNotFound
     }
     
-    //Connected to the right bar button item that was added in viewDidLoad().
-    //Pops up an alert so the user can enter in a word and submit it.
+//Connected to the right bar button item that was added in viewDidLoad().
+//Pops up an alert so the user can enter in a word and submit it.
     @objc func promptForAnswers() {
         let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
         ac.addTextField()
@@ -103,12 +104,12 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
-    //Sets the number of rows in the tableView to the amount of guessed words.
+//Sets the number of rows in the tableView to the amount of guessed words.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usedWords.count
     }
     
-    //Sets the text in each row to an item in the usedWords array.
+//Sets the text in each row to an item in the usedWords array.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         cell.textLabel?.text = usedWords[indexPath.row]
